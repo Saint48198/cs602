84447f5cec6276ca61c381a2312d2abfb3e3f26f
sdaniels <daniels.scott.r@gmail.com>
2016-09-07 19:20:38 -0400

    renamed project sub directory to meet requirements and completed first part.

--- CS602_HW1_daniels/employeeModule.js
+++ CS602_HW1_daniels/employeeModule.js
@@ -18,7 +18,18 @@ class Employees {
     }
 
     addEmployee (fname, lname) {
+        let max = _.max(data, function(employee) { return employee.id });
+        let id = max.id ? max.id + 1 : 0;
+        data.push({ id: id, firstName: fname, lastName: lname });
+    }
 
+    changeEmployeeFirstName (id, fname) {
+        data.some(function (employee) {
+            if (employee.id === id) {
+                employee.firstName = fname;
+                return true;
+            }
+        });
     }
 }
 
@@ -29,3 +40,5 @@ module.exports.lookupById = theEmployees.lookupById;
 module.exports.lookupByLastName = theEmployees.lookupByLastName;
 
 module.exports.addEmployee = theEmployees.addEmployee;
+
+module.exports.changeEmployeeFirstName = theEmployees.changeEmployeeFirstName;
--- CS602_HW1_daniels/hw1a.js
+++ CS602_HW1_daniels/hw1a.js
@@ -7,10 +7,14 @@ console.log(employees.lookupByLastName("Smith"));
 console.log('Adding employee William Smith'.blue);
 employees.addEmployee('William', 'Smith');
 
-console.log('Lookup by last name (Smith)'.blue);
-console.log(employees.lookupByLastName("Smith"));
-
 console.log('Lookup by id (2)'.blue);
 console.log(employees.lookupById(2));
 
 console.log('Changing first name...');
+employees.changeEmployeeFirstName(2, 'Mary');
+
+console.log('Lookup by id (2)'.blue);
+console.log(employees.lookupById(2));
+
+console.log('Lookup by last name (Smith)'.blue);
+console.log(employees.lookupByLastName("Smith"));
