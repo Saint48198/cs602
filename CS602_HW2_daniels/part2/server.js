bb654f14636fd0f153bafa389303ea36b6afc3cb
sdaniels <daniels.scott.r@gmail.com>
2016-09-18 18:15:25 -0400

    hooked up  the employee lookup by id for html, json, and xml

--- CS602_HW2_daniels/part2/server.js
+++ CS602_HW2_daniels/part2/server.js
@@ -29,14 +29,19 @@ app.get('/', (req, res) => {
 });
 
 app.get('/id/:id', (req, res) => {
+    let paramsData = req.params;
+    let id = parseInt(paramsData.id);
+    let intro = 'Employee with the ID ' + id;
+    let employee = employeeModule.lookupById(id);
+
     if (isJson(req.headers.accept)) {
         res.type('application/json');
-        res.send(employeeModule.lookupById(0));
+        res.send(employee);
     } else if (isXML(req.headers.accept)) {
         res.type('application/xml');
-        res.render('xml/employeeList', { layout: __dirname + '/layouts/xml', employee: [employeeModule.lookupById(0)] });
+        res.render('xml/employee', { layout: __dirname + '/layouts/xml', employee: employee });
     } else {
-        res.render('html/employeeList', { employee: [employeeModule.lookupById(0)] });
+        res.render('html/employee', { employee: employee, intro: intro });
     }
 });
 
@@ -44,15 +49,16 @@ app.get('/lastName/:name', (req, res) => {
     let paramsData = req.params;
     let name = paramsData.name;
     let intro = 'Employees with the Last Name ' + name;
+    let employees = employeeModule.lookupByLastName(name);
 
     if (isJson(req.headers.accept)) {
         res.type('application/json');
-        res.send(employeeModule.lookupByLastName(name));
+        res.send(employees);
     } else if (isXML(req.headers.accept)) {
         res.type('application/xml');
-        res.render('xml/employeeList', { layout: __dirname + '/layouts/xml', employee: employeeModule.lookupByLastName(name) });
+        res.render('xml/employeeList', { layout: __dirname + '/layouts/xml', employee: employees });
     } else {
-        res.render('html/employeeList', { employee: employeeModule.lookupByLastName(name), intro: intro });
+        res.render('html/employeeList', { employee: employees, intro: intro });
     }
 });
 
