654b7fe42c79a93a1e3a8bb3177d0cff6744a56f
sdaniels <daniels.scott.r@gmail.com>
2016-09-26 18:16:32 -0400

    added better ui alerts and messages when deleting employees.

--- CS602_HW3_daniels/public/scripts/modal.js
+++ CS602_HW3_daniels/public/scripts/modal.js
@@ -1,19 +1,40 @@
-(function () {
+(function ($, Handlebars) {
     "use strict";
 
     $("#deleteModal").on("show.bs.modal", function (event) {
-        var button = $(event.relatedTarget) // Button that triggered the modal
-        var fname = button.data("fname");
-        var lname = button.data("lname");
-        var id = button.data("id");
+        var $modal = $(this);
 
-        var modal = $(this);
-        var deleteButton = modal.find(".btn-primary");
-        deleteButton.removeAttr("disabled");
+        var $button = $(event.relatedTarget) // Button that triggered the modal
+        var fname = $button.data("fname");
+        var lname = $button.data("lname");
+        var id = $button.data("id"); // needed for the deleteEmployee service
 
-        modal.find(".modal-title").text("Delete Employee: " + lname + ", " + fname);
+        var $modalContent = $(".modal-body", $modal);
+
+        var $cancelBtn = $(".btn-default", $modal);
+        var $deleteButton = $modal.find(".btn-primary", $modal);
+        $deleteButton.removeAttr("disabled");
+
+        // modal content template
+        var source = $("#template-modalMessage").html();
+        var template = Handlebars.compile(source);
+        var defaultHtml = template({});
+
+        // update modal with default content
+        $modalContent.html(defaultHtml); 
+
+        // update modal template title with the Employee full name
+        $modal.find(".modal-title").text("Delete Employee: " + lname + ", " + fname);
+
+        // update modal content with default 
         
-        deleteButton.click(function (e) {
+        $deleteButton.click(function (e) {
+            var $btn = $(this).button('loading');
+
+            // hide the cancel button after the user confirms the deletion of the employee if order to prevent closing the message
+            $cancelBtn.hide();
+
+            // prevent double submittion of the form by checking to see if it disabled and thus working.
             if (!this.hasAttribute("disabled")) {
                 this.setAttribute("disabled", true);
                 $.ajax({
@@ -22,13 +43,20 @@
                     data: { id: id },
                     dataType: "JSON"
                 }).done(function (resp) {
-                    console.log(resp, "success");
-                    window.location.reload(true);
+                    // hard refresh (not from cache) the page to show the updated employee list. 
+                    window.location = "/employees?delete=true";
                 }).fail(function (resp) {
-                    console.log(resp, "failure");
+                    var errorHtml = template({ error: true });
+
+                    // reset form  due to error in service call
+                    $btn.button('reset');
+                    $cancelBtn.show();
+
+                    // update modal content with error message
+                    $modalContent.html(errorHtml);
                 });
             }
         });
     });
 
-})();
+})($, Handlebars);
