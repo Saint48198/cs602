ff2889c989628eebf8b3280ac23782c467d5fb94
sdaniels <daniels.scott.r@gmail.com>
2016-09-30 18:50:46 -0400

    updated to fix issue of not returning single tax value and started to update other functions to return tax value and print them.

--- CS602_HW4_daniels/income_tax_v1.php
+++ CS602_HW4_daniels/income_tax_v1.php
@@ -19,7 +19,10 @@
         // display form page if there is an error otherwise display results
         if ($error_message === '') {
             $displayResults = TRUE;
-            $singleTax = incomeTaxSingle($income);
+            $singleTax = number_format(incomeTaxSingle($income), 2);
+            $marriedJointTax = number_format(incomeTaxMarriedJointly($income), 2);
+            $marriedSeprateTax = number_format(incomeTaxMarriedSeparately($income), 2);
+            $headOfHouseTax = number_format(incomeTaxHeadOfHousehold($income), 2);
         } else {
             $displayResults = FALSE;
         }
@@ -27,24 +30,30 @@
 
 
     function incomeTaxSingle ($value) {
+        $tax = 0;
+
         if ($value <= 9275) {
-            return $value * 0.1;
+            $tax = $value * 0.1;
         } elseif ($value >= 9276 && $value <= 37650) {
-            return 927.50 + (9275 * 0.15);
+            $tax =  927.50 + (($value - 9275) * 0.15);
         } elseif ($value >= 37651 && $value <= 91150) {
-            return 5183.75 + (37650 * 0.25);
+            $tax = 5183.75 + (($value - 37650) * 0.25);
         } elseif ($value >= 91151 && $value <= 190150) {
-            return 18558.75 + (91150 * 0.28);
+            $tax = 18558.75 + (($value - 91150) * 0.28);
         } elseif ($value >= 190151 && $value <= 413350) {
-            return 46278.75 + (190150 * 0.33);
+            $tax = 46278.75 + (($value - 190150) * 0.33);
         } elseif ($value >= 413351 && $value <= 415050) {
-            return 119934.75 + (413350 * 0.35);
-        } elseif ($value <= 415051) {
-            return 120529.75 + (415050 * 0.396);
+            $tax = 119934.75 + (($value - 413350) * 0.35);
+        } elseif ($value >= 415051) {
+            $tax = 120529.75 + (($value - 415050) * 0.396);
         }
+
+        return $tax;
     }
 
     function incomeTaxMarriedJointly ($value) {
+        $tax = 0;
+
         if ($value <= 9275) {
 
         } elseif ($value >= 9276 && $value <= 37650) {
@@ -57,12 +66,16 @@
 
         } elseif ($value >= 413351 && $value <= 415050) {
 
-        } elseif ($value <= 415051) {
+        } elseif ($value >= 415051) {
             
         }
+
+        return $tax;
     }
 
     function incomeTaxMarriedSeparately ($value) {
+        $tax = 0;
+
         if ($value <= 9275) {
 
         } elseif ($value >= 9276 && $value <= 37650) {
@@ -78,9 +91,13 @@
         } elseif ($value <= 415051) {
             
         }
+
+        return $tax;
     }
 
     function incomeTaxHeadOfHousehold ($value) {
+        $tax = 0;
+
         if ($value <= 9275) {
 
         } elseif ($value >= 9276 && $value <= 37650) {
@@ -96,6 +113,8 @@
         } elseif ($value <= 415051) {
             
         }
+
+        return $tax;
     }
 ?>
 <!DOCTYPE html>
@@ -129,38 +148,42 @@
       <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
         <div class="input-group">
             <label for="income">Your Net Income:</label>
-            <input type="text" value="<?php echo $income ?>" name="income" id="income">
+            <input type="text" value="" name="income" id="income">
+            <? if ($error_message) 
+                echo '<div class="error">' . $error_message . '</div>';
+            ?>
         </div>
 
         <button type="submit" class="btn btn-primary">Submit</button>
       </form>
 
       <?php if ($displayResults) { ?>
+      <p>With a net taxable income of <?php echo number_format($income, 2) ?></p>
       <table class="table">
         <thead>
-        <tr>
-            <th>Status</th>
-            <th>Tax</th>
-        </tr>
-        </thead>
-        <tboday>
             <tr>
-                <td>Single<td>
+                <th>Status</th>
+                <th>Tax</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Single</td>
                 <td>$<? echo $singleTax ?></td>
             </tr>
             <tr>
-                <td>Married Filing Jointly<td>
-                <td></td>
+                <td>Married Filing Jointly</td>
+                <td>$<? echo $marriedJointTax ?></td>
             </tr>
             <tr>
-                <td>Married Filing Separately<td>
-                <td></td>
+                <td>Married Filing Separately</td>
+                <td>$<? ech $marriedSeprateTax ?></td>
             </tr>
             <tr>
-                <td>Head of Household<td>
-                <td></td>
+                <td>Head of Household</td>
+                <td>$<? echo $headOfHouseTax ?></td>
             </tr>
-        <tboday>
+        <tbody>
       </table>
       <?php } ?>
 
