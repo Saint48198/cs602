1c01b5e69695491c7ccbf555ea1d72a2afc9c4c2
sdaniels <daniels.scott.r@gmail.com>
2016-10-08 13:29:42 -0400

    updated the html for course list to display courses and created html for the course creation form

---
+++ CS602_HW5_daniels/add_course.php
@@ -0,0 +1,13 @@
+<h2>Add Course</h2>
+
+<form method="post" action="<? echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+    <div class="form-group">
+        <label for="courseId">Course ID</label>
+        <input type="text" class="form-control" id="courseId" name="courseId" maxlength="12">
+    </div>
+    <div class="form-group">
+       <label for="courseName">Course Name</label>
+        <input type="text" class="form-control" id="courseName" name="courseName" maxlength="255">
+    </div>
+    <button type="submit" class="btn btn-primary">Add Course</button>
+</form>
