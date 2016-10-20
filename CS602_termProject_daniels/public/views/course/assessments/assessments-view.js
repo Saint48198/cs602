edd25a14cae70b82e2302ec0101b54e3e38df4f7
sdaniels <daniels.scott.r@gmail.com>
2016-10-20 16:22:58 -0400

    hooked the assignments and assessment detail pages to user the services to get data

--- CS602_termProject_daniels/public/views/course/assessments/assessments-view.js
+++ CS602_termProject_daniels/public/views/course/assessments/assessments-view.js
@@ -8,9 +8,10 @@ define([
 	'util',
 	'../../../collections/assessment-collection',
 	'../../../models/course-model',
+	'../../../models/assessment-model',
 	'base'
 
-], function ($, _, Backbone, UTIL, AssessmentCollection, CourseModel) {
+], function ($, _, Backbone, UTIL, AssessmentCollection, CourseModel, AssessmentModel) {
 	'use strict';
 	var CourseAssessmentsView = BaseView.fullExtend({
 
@@ -23,21 +24,43 @@ define([
 		onInitialize: function () {
 			this.assessmentCollection = new AssessmentCollection();
 			this.courseModel = new CourseModel();
+			this.assessmentModel = new AssessmentModel();
 		},
 
 		onRender: function (actions) {
 			var courseId = actions.type;
+			var query = UTIL.QueryString();
+			var assessmentId = query.assessment_id;
 
-			this.assessmentCollection.url = this.assessmentCollection.url.split('?')[0] + '?course_id=' + courseId;
+
+			this.replaceUsingTemplate('template-courseAssessmentsBase', this.$el, { courseId: courseId }, { title: 'Course - ' + courseId + ' Assessments' });
 			this.courseModel.url = this.courseModel.existingCourseUrl + courseId;
 
-			Promise.all([this.assessmentCollection.fetch(), this.courseModel.fetch()]).then(function (fullfill, reject) {
-				if (fullfill.length) {
-					this.replaceUsingTemplate('template-courseAssessments', this.$el, { courseId: courseId, assessment: this.assessmentCollection.toJSON(), course: this.courseModel.toJSON() }, { title: 'Course - ' + courseId + ' Assessments' });
-				} else {
-					alert('error');
-				}
-			}.bind(this));
+			if (assessmentId) {
+				this.assessmentModel.url = this.assessmentModel.existingAssessmentUrl + assessmentId;
+				Promise.all([this.courseModel.fetch(), this.assessmentModel.fetch()]).then(function (fullfill, reject) {
+					if (fullfill.length) {
+						this.displayContent({ courseId: courseId, assessment: this.assessmentModel.toJSON(), course: this.courseModel.toJSON() }, 'template-courseAssessment')
+
+					} else {
+						alert('error');
+					}
+				}.bind(this));
+			} else {
+				this.assessmentCollection.url = this.assessmentCollection.url.split('?')[0] + '?course_id=' + courseId;
+				Promise.all([this.assessmentCollection.fetch(), this.courseModel.fetch()]).then(function (fullfill, reject) {
+					if (fullfill.length) {
+						this.displayContent({ courseId: courseId, assessment: this.assessmentCollection.toJSON(), course: this.courseModel.toJSON() }, 'template-courseAssessments')
+
+					} else {
+						alert('error');
+					}
+				}.bind(this));
+			}
+		},
+
+		displayContent: function (data, templateId) {
+			this.replaceUsingTemplate(templateId, $('#courseContent', this.$el), data);
 		}
 	});
 	return CourseAssessmentsView;
--- CS602_termProject_daniels/public/views/course/assignments/assignments-view.js
+++ CS602_termProject_daniels/public/views/course/assignments/assignments-view.js
@@ -8,9 +8,10 @@ define([
 	'util',
 	'../../../collections/assignment-collection',
 	'../../../models/course-model',
+	'../../../models/assignment-model',
 	'base'
 
-], function ($, _, Backbone, UTIL, AssignmentCollection, CourseModel) {
+], function ($, _, Backbone, UTIL, AssignmentCollection, CourseModel, AssignmentModel) {
 	'use strict';
 	var CourseAssignmentsView = BaseView.fullExtend({
 
@@ -23,22 +24,44 @@ define([
 		onInitialize: function () {
 			this.assignmentCollection = new AssignmentCollection();
 			this.courseModel = new CourseModel();
+			this.assignmentModel = new AssignmentModel();
 		},
 
 		onRender: function (actions) {
 			var courseId = actions.type;
+			var query = UTIL.QueryString();
+			var assignmentId = query.assignment_id;
 
-			this.assignmentCollection.url = this.assignmentCollection.url.split('?')[0] + '?course_id=' + courseId;
 			this.courseModel.url = this.courseModel.existingCourseUrl + courseId;
+			this.replaceUsingTemplate('template-courseAssignmentsBase', this.$el, { courseId: courseId }, { title: 'Course - ' + courseId + 'Assignment' });
 
 
-			Promise.all([this.assignmentCollection.fetch(), this.courseModel.fetch()]).then(function (fullfill, reject) {
-				if (fullfill.length) {
-					this.replaceUsingTemplate('template-courseAssignments', this.$el, { courseId: courseId, assignment: this.assignmentCollection.toJSON(), course: this.courseModel.toJSON() }, { title: 'Course - ' + courseId + 'Assignment' });
-				} else {
 
-				}
-			}.bind(this));
+			if (assignmentId) {
+				this.assignmentModel.url = this.assignmentModel.existingAssignmentUrl + assignmentId;
+				Promise.all([this.assignmentModel.fetch(), this.courseModel.fetch()]).then(function (fullfill, reject) {
+					if (fullfill.length) {
+						this.displayContent({ courseId: courseId, assignment: this.assignmentModel.toJSON(), course: this.courseModel.toJSON() }, 'template-courseAssignment');
+					} else {
+						alert('error');
+					}
+				}.bind(this));
+			} else {
+				this.assignmentCollection.url = this.assignmentCollection.url.split('?')[0] + '?course_id=' + courseId;
+				Promise.all([this.assignmentCollection.fetch(), this.courseModel.fetch()]).then(function (fullfill, reject) {
+					if (fullfill.length) {
+						this.displayContent({ courseId: courseId, assignment: this.assignmentCollection.toJSON(), course: this.courseModel.toJSON() }, 'template-courseAssignments');
+					} else {
+						alert('error');
+					}
+				}.bind(this));
+			}
+
+
+		},
+
+		displayContent: function (data, templateId) {
+			this.replaceUsingTemplate(templateId, $('#courseContent', this.$el), data);
 		}
 	});
 	return CourseAssignmentsView;
