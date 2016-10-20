1591e0139613f167b3bf4064d4dd616649fd4c3f
sdaniels <daniels.scott.r@gmail.com>
2016-10-20 07:52:46 -0400

    created the home page for courses for users

--- CS602_termProject_daniels/public/views/course/course-view.js
+++ CS602_termProject_daniels/public/views/course/course-view.js
@@ -1,13 +1,16 @@
-// Page Section: Sample
-// Filename: sample-view.js
+// Page Section: Course
+// Filename: course-view.js
 
 define([
 	'jquery',
 	'underscore',
 	'backbone',
+	'util',
+	'../../collections/module-collection',
+	'../../models/course-model',
 	'base'
 
-], function ($, _, Backbone, base) {
+], function ($, _, Backbone, UTIL, ModuleCollection, CourseModel) {
 	'use strict';
 	var CourseView = BaseView.fullExtend({
 
@@ -18,11 +21,24 @@ define([
 		},
 
 		onInitialize: function () {
-
+			this.moduleCollection = new ModuleCollection();
+			this.courseModel = new CourseModel();
 		},
 
-		onRender: function () {
-			this.replaceUsingTemplate('template-course', this.$el, {}, { title: 'Course' });
+		onRender: function (actions) {
+			var courseId = actions.type;
+
+			this.moduleCollection.url = this.moduleCollection.url.split('?')[0] + '?course_id=' + courseId;
+			this.courseModel.url = this.courseModel.existingCourseUrl + courseId;
+
+
+			Promise.all([this.moduleCollection.fetch(), this.courseModel.fetch()]).then(function (fullfill, reject) {
+				if (fullfill.length) {
+					this.replaceUsingTemplate('template-course', this.$el, { courseId: courseId, module: this.moduleCollection.toJSON(), course: this.courseModel.toJSON() }, { title: 'Course - ' + courseId });
+				} else {
+
+				}
+			}.bind(this));
 		}
 	});
 	return CourseView;
